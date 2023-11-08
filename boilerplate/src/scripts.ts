475b23f656509b12afede09ec2173ea556cebf42
Egils <63558651+looppng@users.noreply.github.com>
2023-11-08 14:52:36 +0200

    49-55

--- boilerplate/src/scripts.ts
+++ boilerplate/src/scripts.ts
@@ -40,7 +40,7 @@ console.log(whatType(['array']));
 // Write a function that takes two values, say a and b, as arguments
 // Return true if the two values are equal and of the same type
 
-const compareValueType = (a:any, b:any) : boolean => {
+const compareValueType = (a:string | number, b:string | number) : boolean => {
     return a === b;
 }
 
@@ -948,18 +948,6 @@ console.log(getKey('b'));
 // Create an object that has a property with key 'a' and a value of 'b'
 // Return the object
 
-// myFunction('a','b')
-// Expected
-// {a:'b'}
-
-// myFunction('z','x')
-// Expected
-// {z:'x'}
-
-// myFunction('b','w')
-// Expected
-// {b:'w'}
-
 const createObject = (key: string, value: string): Record<string, string> => {
     return { [key]: value };
 }
@@ -968,6 +956,139 @@ console.log("49.uzd");
 console.log(createObject('a','b'));
 console.log(createObject('z','x'));
 console.log(createObject('b','w'));
+
+
+// Write a function that takes two arrays (a and b) as arguments
+// Create an object that has properties with keys 'a' and corresponding values 'b'
+// Return the object
+
+const createObjectFromArrays = (a: (string | number)[], b: (string | number)[]): Record<string | number, string | number> => {
+    const result: Record<string | number, string | number> = {};
+  
+    for (let i = 0; i < a.length; i++) {
+      result[a[i]] = b[i];
+    }
+  
+    return result;
+}  
+
+console.log("50.uzd");
+console.log(createObjectFromArrays(['a','b','c'],[1,2,3]));
+console.log(createObjectFromArrays(['w','x','y','z'],[10,9,5,2]));
+console.log(createObjectFromArrays([1,'b'],['a',2]));
+
+
+// Write a function that takes an object (a) as argument
+// Return an array with all object keys
+
+const getObjectKeys = <T>(obj: Record<string, T>): string[] => {
+    return Object.keys(obj);
+  }
+  
+console.log("51.uzd");
+console.log(getObjectKeys({a:1,b:2,c:3}));
+console.log(getObjectKeys({j:9,i:2,x:3,z:4}));
+console.log(getObjectKeys({w:15,x:22,y:13}));
+
+
+// Write a function that takes an object (a) as argument
+// Return the sum of all object values
+
+const sumOfObjects = <T extends number>(obj: Record<string, T>): number => {
+    const values = Object.values(obj);
+    return values.reduce((a, b) => a + b, 0);
+}
+  
+console.log("52.uzd");
+console.log(sumOfObjects({a:1,b:2,c:3}));
+console.log(sumOfObjects({j:9,i:2,x:3,z:4}));
+console.log(sumOfObjects({w:15,x:22,y:13}));
+
+
+// Write a function that takes an object as argument
+// It should return an object with all original object properties
+// except for the property with key 'b'
+
+const filterKey = <T>(obj: Record<string, T>): Record<string, T> => {
+    return Object.fromEntries(Object.entries(obj).filter(([key]) => key!== 'b'));
+}
+
+console.log("53.uzd");
+console.log(filterKey({ a: 1, b: 7, c: 3 }));
+console.log(filterKey({ b: 0, a: 7, d: 8 }));
+console.log(filterKey({ e: 6, f: 4, b: 5, a: 3 }));
+
+
+// Write a function that takes two objects as arguments
+// Unfortunately, the property 'b' in the second object has the wrong key
+// should be named 'd' instead
+// Merge both objects and correct the wrong property name
+// Return the resulting object
+// It should have the properties 'a', 'b', 'c', 'd', and 'e'
+
+const combObj = <T>(obj1: Record<string, T>, obj2: Record<string, T>): Record<string, T> => {
+    // Create a copy of obj2
+    const modifiedObj2 = { ...obj2 };
+  
+    // Rename 'b' to 'd' in the copy of obj2
+    if ('b' in modifiedObj2) {
+      modifiedObj2['d'] = modifiedObj2['b'];
+      delete modifiedObj2['b'];
+    }
+  
+    // Merge the modified copy of obj2 with obj1 and return the result
+    return Object.fromEntries(
+      Object.entries(obj1).concat(Object.entries(modifiedObj2))
+    );
+}
+
+console.log("54.uzd");
+console.log(combObj({ a: 1, b: 2 }, { c: 3, b: 4, e: 5 }));
+console.log(combObj({ a: 5, b: 4 }, { c: 3, b: 1, e: 2 }));
+
+
+// Write a function that takes an object (a) and a number (b) as arguments
+// Multiply all values of 'a' by 'b'
+// Return the resulting object
+
+// myFunction({a:1,b:2,c:3},3)
+// Expected
+// {a:3,b:6,c:9}
+
+// myFunction({j:9,i:2,x:3,z:4},10)
+// Expected
+// {j:90,i:20,x:30,z:40}
+
+// myFunction({w:15,x:22,y:13},6)
+// Expected
+// {w:90,x:132,y:78}
+
+const multiplyObjectValues = <T extends number>(obj: Record<string, T>, multiplier: number): Record<string, number> => {
+    const result: Record<string, number> = {};
+  
+    for (const key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        if (typeof obj[key] === 'number') {
+          result[key] = obj[key] * multiplier;
+        }
+      }
+    }
+  
+    return result;
+}
+
+console.log("55.uzd");
+console.log(multiplyObjectValues({a:1,b:2,c:3},3));
+console.log(multiplyObjectValues({j:9,i:2,x:3,z:4},10));
+console.log(multiplyObjectValues({w:15,x:22,y:13},6));
+
+
+
+
+
+
+
+
 
 
 
